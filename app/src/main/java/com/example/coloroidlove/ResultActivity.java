c0d426c11cd22d937ac156f861dfe0cee3320c6c
Haejeong Cho <s2036@e-mirim.hs.kr>
2022-06-20 14:51:17 +0900

    설명글 수정

--- app/src/main/java/com/example/coloroidlove/ResultActivity.java
+++ app/src/main/java/com/example/coloroidlove/ResultActivity.java
@@ -5,10 +5,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.example.coloroidlove.CameraActivity;
+
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 
@@ -27,9 +26,9 @@ public class ResultActivity  extends BaseActivity {
     int index;
 
     // 결과 프로필 배열
-    Integer[] reusltWarmImg = {R.drawable.result_springlight, R.drawable.result_springlight,
+    Integer[] resultWarmImg = {R.drawable.result_springlight, R.drawable.result_springlight,
             R.drawable.result_fallmute, R.drawable.result_fallstrong, R.drawable.result_falldeep}; // 웜 결과 이미지
-    Integer[] reusltCoolImg = {R.drawable.result_summerlight, R.drawable.result_summermute, R.drawable.result_summerbright, R.drawable.result_summerlowbrightmute,
+    Integer[] resultCoolImg = {R.drawable.result_summerlight, R.drawable.result_summermute, R.drawable.result_summerbright, R.drawable.result_summerlowbrightmute,
             R.drawable.result_wintertrue,   R.drawable.result_winterbright,  R.drawable.result_winterdeep}; // 쿨 결과 이미지
 
     // 결과 자세한 설명
@@ -44,19 +43,19 @@ public class ResultActivity  extends BaseActivity {
 
     // 출력
     String[] WarmDesc = {
-            "파스텔 톤 컬러 중에서 맑고 깨끗한 느낌의 컬러~\n" + "살랑살랑하고 깨끗한 느낌이 도는\n마치 솜사탕 같은 컬러가 찰떡!",
-            "또렷하고 생기 있는 이미지가 강하다!\n" +"\n" +"피부 톤이 매우 밝고, 비비드한 컬러가 잘 어울리는 것이 특징!",
-            "가을 뮤트는 스트롱톤보다는 더 부드러운, 중간 밝기, 중간 채도의 색상이 더 잘 어울리는 타입이다. 부드럽고 은은한 이미지로 ‘크림 라떼’가 떠올라요!",
-            "가을 스트롱은 컬러 스펙트럼이 가장 넓은 톤이다. 워스트 컬러가 거의 없다고 볼 수 있는 엄청난 톤! 스트롱 웜톤인 만큼 옐로 베이스를 픽하세요!",
-            "어두운 색상이 잘어울리는 타입! 원색에 검은색이 섞인 낮은 명채도의 색들로 구성되어 있고 대체로 섹시하고 고급스러운 분위기~. "};
+            "파스텔 톤 컬러 중에서 맑고 깨끗한 느낌의 컬러~\n살랑살랑하고 깨끗한 느낌이 도는\n마치 솜사탕 같은 컬러가 찰떡!",
+            "또렷하고 생기 있는 이미지가 강하다!\n피부 톤이 매우 밝고,\n비비드한 컬러가 잘 어울리는 것이 특징!",
+            "가을 뮤트는 스트롱톤보다는 더 부드러운\n중간 밝기, 중간 채도의 색상이 더 잘 어울리는 타입\n 부드럽고 은은한 이미지로 ‘크림 라떼’가 떠올라요!",
+            "가을 스트롱은 컬러 스펙트럼이 가장 넓은 톤\n 워스트 컬러가 거의 없다고 볼 수 있는 엄청난 톤!\n 스트롱 웜톤인 만큼 옐로 베이스를 픽하세요!",
+            "어두운 색상이 잘어울리는 타입!\n원색에 검은색이 섞인 낮은 명채도의 색들로 구성되어 있고\n 대체로 섹시하고 고급스러운 분위기"};
     String[] CoolDesc = {
             "회색 기와 푸른 기가 살짝 도는 고명도의 \n은은한 파스텔 컬러" + "맑고, 청량하고, 싱그러운 느낌!",
             "회색 기가 많이 섞인 톤 다운된 파스텔 계열이 찰떡!\n" + "\n" + "우아+단아한 분위기는 세련되고 시크한 이미지까지",
-            "여름 브라이트는 여름 ‘트루’ 톤이라고도 하는데, 생기 가득하면서 청량한 느낌을 가진 톤이이며 채도가 높은 원색 계열이 잘 받는 톤!",
-            "쿨한 느낌이지만 대비가 강하지 않고 그레이가 섞인 부드러운 이미지 소프트 서머를 대표 하는 컬러로는 명도가 높고 채도가 낮은 컬러!",
-            "창백한 느낌의 피부와 흑단 같은 헤러컬러로 백설공주 분위기~ 카리스마 있고 도시적인 스타일링이 가능하고 블랙&화이트로 코디하면 찰떡!!",
-            "겨울 쿨톤 중에서 가장 화려한 타입이며\n쿨 베이스의 고채도 컬러나 선명한 컬러로 스타일링을 하는게 베스트! 쿨톤의 시크한 매력을 살리기에 딱!",
-            "뭔가 멋있어보이는 분위기를 가진...! 자칭 걸크러쉬분들이 떠오르는 퍼스널 컬러이다! 다크함이 매력인 겨울딥은 낮은 채도 컬러의 포인트 컬러로 얼굴빛 살아나요!."
+            "여름 브라이트는 여름 ‘트루’ 톤이라고도 하는데,\n생기 가득하면서 청량한 느낌을 가진 톤이이며\n 채도가 높은 원색 계열이 잘 받는 톤!",
+            "쿨한 느낌이지만 대비가 강하지 않고\n그레이가 섞인 부드러운 이미지 소프트 서머를 대표 하는 컬러로\n명도가 높고 채도가 낮은 컬러!",
+            "창백한 느낌의 피부와 흑단 같은 헤어컬러로\n백설공주 분위기~ 카리스마 있고 도시적인 스타일링이 가능하고\n블랙&화이트로 코디하면 찰떡!!",
+            "겨울 쿨톤 중에서 가장 화려한 타입이며\n쿨 베이스의 고채도 컬러나 선명한 컬러로 스타일링을 하는게 베스트!\n쿨톤의 시크한 매력을 살리기에 딱!",
+            "뭔가 멋있어보이는 분위기를 가진...!\n자칭 걸크러쉬분들이 떠오르는 퍼스널 컬러이다! 다크함이 매력인 겨울딥은\n낮은 채도 컬러의 포인트 컬러로 얼굴빛 살아나요!."
     };
 
     // 대표적인 연예인
@@ -88,8 +87,8 @@ public class ResultActivity  extends BaseActivity {
                 "흑발, 로즈골드 악세사리", // 여름뮤트
                 "핑크계열 블러셔, 하얀색 계열의 옷", // 여름브라이트
                 "핑크베이스의 파운데이션 매트한 립", // 여름저명도뮤트
-                "블랙 헤어 컬러, 대비가 강한 스타일링", // 겨울트루
-                "딥블랙 헤어 컬러, 화려한 스타일링 ", // 겨울브라이트
+                "블랙 헤어 컬러,대비강한 스타일", // 겨울트루
+                "딥블랙 헤어 컬러, 화려한 스타일 ", // 겨울브라이트
                 "가죽 소재 옷, 블랙&화이트룩" // 겨울딥
     };
 
@@ -138,13 +137,13 @@ public class ResultActivity  extends BaseActivity {
         res_title.setText(personal);
 
         if(base==0){ // warm
-            res_img.setImageDrawable(ContextCompat.getDrawable(this, reusltWarmImg[index]));
+            res_img.setImageDrawable(ContextCompat.getDrawable(this, resultWarmImg[index]));
             res_desc.setText(WarmDesc[index]);
             res_artist.setText("대표적인 연예인 : " + WarmArtist[index]);
             res_item.setText("추천 아이템 : " + WarmItem[index]);
             res_color.setText("추천 컬러 : " + WarmColor[index]);
         }else{ // cool
-            res_img.setImageDrawable(ContextCompat.getDrawable(this, reusltCoolImg[index]));
+            res_img.setImageDrawable(ContextCompat.getDrawable(this, resultCoolImg[index]));
             res_desc.setText(CoolDesc[index]);
             res_artist.setText("대표적인 연예인 : " + CoolArtist[index]);
             res_item.setText("추천 아이템 : " + CoolItem[index]);
